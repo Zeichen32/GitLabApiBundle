e9a9f9d83dfaa3084842e173954b16afbf0e6f19
Alex McCabe <alex@alexdmccabe.com>
2023-10-09 07:45:06 -0400

    Add TreeBuilder return type. (#27)

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -24,7 +24,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritDoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('zeichen32_git_lab_api');
 
