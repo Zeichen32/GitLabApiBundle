976eccc3bd63761ae514082c9959808a5e2ef2fc
Bram Pauwels <bram@zimmo.be>
2026-02-12 09:06:26 +0100

    Add support for Symfony 8

--- DependencyInjection/Zeichen32GitLabApiExtension.php
+++ DependencyInjection/Zeichen32GitLabApiExtension.php
@@ -33,8 +33,8 @@ class Zeichen32GitLabApiExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new Loader\PhpFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.php');
 
         // Create default Buzz HttpClient
        // $httpClient = new Definition('Buzz\Client\Curl');
